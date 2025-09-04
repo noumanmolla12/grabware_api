0afe2cb00a603b65b4b286dafbecc9051de3ea8c
Mohammad Nouman MOLLA <101586016+noumanmolla12@users.noreply.github.com>
2025-09-04 16:07:47 +0530

    Initial commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // ✅ CORS setup
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://grabware-frontend-woad.vercel.app'],
   credentials: true
 }));
 
